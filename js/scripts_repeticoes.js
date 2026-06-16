070b4c2b69707c7d2afe71fc99b102087c142fbb
marcella <marcellamatos2428@gmail.com>
2026-06-16 16:42:46 -0300

    div-cont-acum/  contador/acumulador

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -3,4 +3,11 @@ const divFor = document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++ ){
     console.log(i, 'amor')
+    divFor.innerHTML += `${i} - Amor <br>`
 }
+//contador/acumulador
+
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
