f722a1dbc7266aaf98e2c036b39fcf49984b47d2
marcella <marcellamatos2428@gmail.com>
2026-06-16 16:53:08 -0300

    cont++

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -11,3 +11,19 @@ const inputNum = document.querySelector('#num')
 const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
 
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click ', (evt)=>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+     divContAcum.innerHTML = `total de numero digitado é: ${cont} <br>
+     a soma dos numeros é: ${acum}`
+
+     inputNum.value = ''
+
+
+})
+
