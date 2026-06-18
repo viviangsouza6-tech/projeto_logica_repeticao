80344ba57ea05b2a9a01d8eb148d6f2577997780
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-18 14:52:43 -0300

    arquivo commitado

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -2,7 +2,7 @@
 const divFor = document.querySelector('#div-for')
 
 for( i = 0; i < 10; i++){
-    divFor.innerHTML += `${i} - Amor <br>`
+    divFor.innerHTML += `${i + 1} - Amor <br>`
 }
 
 //CONTADOR/ACUMULADOR
@@ -35,16 +35,21 @@ btnFrase.addEventListener('click', (evt)=>{
     let frase = inputFrase.value
 
     for(i = 0; i < numRepeti; i++){
-        divFrase.innerHTML += `${i} - ${frase} <br>`
+        divFrase.innerHTML += `${i + 1} - ${frase} <br>`
     }
 })
 
 //COLEÇÃO DE DADOS - ARRAY
 const presentes = ['Bicicleta', 'celular', 'camisa do Mengão', 'Doleira', 'Tenis Adidas']
 
+console.log(presentes[2])
+
+presentes [3] = 'Relógio'
+
 const divArray = document.querySelector('#div-array')
 
 for(i =0; i < 5;i++){
+    console.log(presentes[i])
     divArray.innerHTML +=  `${presentes[i]} <br>`
 }
 
@@ -69,6 +74,7 @@ presentes.forEach((elemento, posicao)=>{
     divForeach.innerHTML += `${posicao} - ${elemento} <br>`
 })
 
+//COLEÇÃO DE OBJETOS LITERAIS ARRAY
 const pessoas = [
     {nome: 'Maria Flor', idade : 25, renda:8500},
     {nome: 'Joerdson Souza', idade : 75, renda:5000},
@@ -80,6 +86,26 @@ const pessoas = [
 //LISTANDO OBJETO LITERAL PELO FOR 
 const divListaObjFor = document.querySelector('#div-listado-for')
 
-for(i = 0; i < pessoas.length;i++){
-    divListaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i].idade}, ${pessoas[i].renda.toFixed(2).replace('.',',')} <br>`
+for(let indice in pessoas){
+    divListaObjForIn.innerHTML += `${indice} - ${pessoas [indice].nome}, ${pessoas [indice].idade}, R$ ${pessoas [indice].renda.toFixed(2).replace('.',',')} <br>`
 }
+
+//LISTANDO OBJETO LITERAL PELO FOR/OF
+const divListaObjForOf = document.querySelector
+('#div-listaobjt-forof')
+
+let contForOf = 0
+
+for (let elem of pessoas){
+    contForOf++
+
+    divListaObjForOf.innerHTML += `${contForOf} - ${elem.nome}, ${elem.idade}, R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
+}
+
+//LISTANDO OBJETO LITERAL PELO FOREACH
+const divListaObjForEach = document.querySelector
+('#div-lista-foreach')
+
+pessoas.forEach((elem, i)=>{
+    divListaObjForEach.innerHTML += `${i + 1}, ${elem.nome}, ${elem.idade} R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
+})
