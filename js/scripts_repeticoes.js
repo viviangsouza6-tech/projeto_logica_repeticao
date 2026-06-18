8d680340e5ed1402ec4cb24d6529593061a3b238
Vivian <Vivian.g.souza6@aluno.senai.br>
2026-06-18 16:28:59 -0300

    arquivo commitado

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,8 +1,9 @@
-//ESTRUTURA DE REPETIÇAO
-const divFor = document.querySelector('#div-for')
+//ESTRUTURA DE REPETIÇÃO FOR
+const divfor = document.querySelector('#div-for')
 
-for( i = 0; i < 10; i++){
-    divFor.innerHTML += `${i + 1} - Amor <br>`
+for(i = 0;i < 10; i++){
+    console.log(i, 'Amor')
+    divfor.innerHTML += `${i} - Amor <br>`
 }
 
 //CONTADOR/ACUMULADOR
@@ -13,44 +14,43 @@ const divContAcum = document.querySelector('#div-cont-acum')
 let cont = 0
 let acum = 0.0
 
-btnNum.addEventListener('click', (evt)=>{
+btnNum.addEventListener('click' , (evt)=>{
     let numDigitado = Number(inputNum.value)
 
     cont++
     acum += numDigitado
 
-    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br> A SOMA DOS NÚMERO É: ${acum}`
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br> A SOMA DOS NÚMEROS É: ${acum}`
 
     inputNum.value = ''
 })
 
-//CONTROLADOR FOR
+
+
+//CONTROLANDO FOR
 const inputFrase = document.querySelector('#frase')
 const inputNumRepeticao = document.querySelector('#num-repeticao')
 const btnFrase = document.querySelector('#btn-frase')
 const divFrase = document.querySelector('#div-frase')
 
-btnFrase.addEventListener('click', (evt)=>{
+btnFrase.addEventListener('click', (evt) =>{
     let numRepeti = Number(inputNumRepeticao.value)
     let frase = inputFrase.value
 
-    for(i = 0; i < numRepeti; i++){
+    for (i = 0; i < numRepeti; i++){
         divFrase.innerHTML += `${i + 1} - ${frase} <br>`
     }
 })
 
-//COLEÇÃO DE DADOS - ARRAY
-const presentes = ['Bicicleta', 'celular', 'camisa do Mengão', 'Doleira', 'Tenis Adidas']
-
-console.log(presentes[2])
-
-presentes [3] = 'Relógio'
+//COLEÇÃO DE DADOS  ARRAY
+const presentes = ['Bicicleta', 'Camiseta', 'Mochila', 'Tênis','Celular']
 
 const divArray = document.querySelector('#div-array')
 
-for(i =0; i < 5;i++){
-    console.log(presentes[i])
-    divArray.innerHTML +=  `${presentes[i]} <br>`
+//divArray.innerHTML = presente[6]
+
+for(i = 0;i < 5;i++ ){
+    divArray.innerHTML += `${presentes[i]} <br>`
 }
 
 //FOR IN
@@ -58,12 +58,12 @@ const divForIn = document.querySelector('#div-forin')
 
 for(let posicao in presentes){
     divForIn.innerHTML += `${presentes[posicao]} <br>`
-    
 }
-//FOR OF 
+
+//FOR OF
 const divForOf = document.querySelector('#div-forof')
 
-for(let elemento of presentes){
+for (let elemento of presentes){
     divForOf.innerHTML += `${elemento} <br>`
 }
 
@@ -74,38 +74,69 @@ presentes.forEach((elemento, posicao)=>{
     divForeach.innerHTML += `${posicao} - ${elemento} <br>`
 })
 
-//COLEÇÃO DE OBJETOS LITERAIS ARRAY
+//COLEÇÂO DE OBJETOS LITERAIS - ARRAY
 const pessoas = [
-    {nome: 'Maria Flor', idade : 25, renda:8500},
-    {nome: 'Joerdson Souza', idade : 75, renda:5000},
-    {nome: 'Taoca', idade : 28, renda:500},
-    {nome: 'Chicó', idade : 26, renda:100},
-    {nome: 'João Grilo', idade : 32, renda:80},
-       
+    {nome: 'Maria Flor', idade: 25, renda:8500},
+    {nome: 'Joerdson Souza', idade: 75, renda:5000},
+    {nome: 'Taoca', idade: 28, renda:500},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'João Grilo', idade: 32, renda:80},
 ]
-//LISTANDO OBJETO LITERAL PELO FOR 
-const divListaObjFor = document.querySelector('#div-listado-for')
+
+//MANIPULANDO ARRAY
+//ADICIONAR ELEMENTOS NO ARRAY
+pessoas.push({nome: 'Magnólia', idade: 32, renda:3500})
+pessoas.unshift({nome: 'Joerdison', idade:36, renda: 100000})
+
+//SPLICE PARA ADICIONAR EM UMA POSIÇÂO SEM EXCLUIR UM ELEMENTO
+pessoas.splice(3,0,{nome: 'Extraterrestre', idade: 250, renda: 0.50})
+
+//SPLICE ADICIONA EM UMA POSIÇÂO E NESSE EXEMPLO EXCLUI MAIS DOIS ELEMENTOS A PARTIR DA POSIÇÂO(INDÌCE) INFORMADO
+pessoas.splice(3,2,{nome: 'Extraterrestre', idade: 250, renda: 0.50})
+
+//SPLICE EXCLUI UMA POSÇÃO E NESSE EXEMPLO EXCLUI APENAS O ELEMNTO DA POSIÇÃO(INDÍCE) INFORMADA
+pessoas.splice(1,0)
+
+//SLIPECE EXCLUI DOIS ELEMENTOS A PARTIR DA POSIÇÃO(INDÍCE) INFORMADA
+pessoas.splice(2,2)
+
+//EXCLUIR O ÚLTIMO ELEMENTO DO ARRAY
+pessoas.pop()
+
+//EXIBIR A QUANTIDADE DE ELEMENTOS DO ARRAY
+console.log('TOTAL DE ELEMENTOS NO ARRAY pessoas ====> ',pessoas.length)
+
+//LISTANDO OBJETO LITERAL PELO FOR
+const divListaObjFor = document.querySelector('#div-listaobj-for')
+
+for(i = 0; i < pessoas.length;i++){
+    divListaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i].idade} R$ ${pessoas[i].renda.toFixed(2).replace('.', ',')} <br>`
+}
+
+//LISTANDO OBJETO LITERAL PELO FOR/IN
+const divListaObjForIn = document.querySelector('#div-listaobj-forin')
 
 for(let indice in pessoas){
-    divListaObjForIn.innerHTML += `${indice} - ${pessoas [indice].nome}, ${pessoas [indice].idade}, R$ ${pessoas [indice].renda.toFixed(2).replace('.',',')} <br>`
+    divListaObjForIn.innerHTML += `${indice} - ${pessoas
+    [indice].nome}, ${pessoas[indice].idade}, R$ ${pessoas[indice].renda.toFixed(2).replace('.',',')} <br>`
+
 }
 
 //LISTANDO OBJETO LITERAL PELO FOR/OF
-const divListaObjForOf = document.querySelector
-('#div-listaobjt-forof')
+const divListaObjForOF =document.querySelector('#div-listaobj-forof')
 
 let contForOf = 0
 
-for (let elem of pessoas){
+for(let elem of pessoas){
     contForOf++
 
-    divListaObjForOf.innerHTML += `${contForOf} - ${elem.nome}, ${elem.idade}, R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
+    divListaObjForOF.innerHTML += `${contForOf} - ${elem.nome}, ${elem.idade}, R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
 }
 
 //LISTANDO OBJETO LITERAL PELO FOREACH
-const divListaObjForEach = document.querySelector
+const divListaForeach = document.querySelector
 ('#div-lista-foreach')
 
 pessoas.forEach((elem, i)=>{
-    divListaObjForEach.innerHTML += `${i + 1}, ${elem.nome}, ${elem.idade} R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
+    divListaForeach.innerHTML += `${i + 1} - ${elem.nome}, ${elem.idade} R$ ${elem.renda.toFixed(2).replace('.',',')}<br>`
 })
